d22e7ae1157adbd8afa522bb651439495281be39
yibaebi <elliot.yibaebi@yahoo.com>
2025-07-07 23:40:00 +0100

    setup translation graph nodes and chains

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,15 +1,25 @@
 from pydantic_settings import BaseSettings
 import os
 
+
 class Settings(BaseSettings):
     PROJECT_NAME: str = os.getenv("PROJECT_NAME")
     VERSION: str = os.getenv("VERSION")
     HF_TOKEN: str = os.getenv("HF_TOKEN")
     CORS_ORIGINS: str = os.getenv("CORS_ORIGINS")
-    DEBUG: bool = os.getenv("DEBUG")
+    DEBUG: bool = os.getenv("DEBUG", False)
+    OLLAMA_HOST: str = os.getenv("OLLAMA_HOST")
+
+    # LangChain settings
+    LANGCHAIN_TRACING_V2: bool = os.getenv("LANGCHAIN_TRACING_V2", False)
+    LANGSMITH_API_KEY: str = os.getenv("LANGSMITH_API_KEY", "")
+    LANGSMITH_PROJECT: str = os.getenv("LANGSMITH_PROJECT", "")
+    LANGSMITH_ENDPOINT: str = os.getenv("LANGSMITH_ENDPOINT", "")
+    LANGSMITH_TRACING: bool = os.getenv("LANGSMITH_TRACING", False)
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+
 
 settings = Settings()
